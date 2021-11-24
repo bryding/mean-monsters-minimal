b288e347af91d1cfe9c0e498252c208f6075aeab
Felix Winterhalter <felix@arfinity.io>
2021-11-24 18:01:19 +0100

    Refactor to .net6.0 aspnetcore

--- mean-monsters-server/Program.cs
+++ mean-monsters-server/Program.cs
@@ -1,44 +1,46 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Grpc.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace mean_monsters_server
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+
+// Add services to the container.
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseGrpcWeb(new GrpcWebOptions()
 {
-  class GameServerImpl : GameService.GameServiceBase
-  {
+    DefaultEnabled = true
+});
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<GameServerImpl>();
+});
+
+var task = app.RunAsync();
+Console.WriteLine("Press any key to stop the server...");
+Console.ReadKey();
+app.StopAsync();
+
+class GameServerImpl : GameService.GameServiceBase
+{
     public override Task<FullMonsterStateResponse> getMatchResult(GetMatchResultRequest request,
       ServerCallContext context)
     {
-      Console.WriteLine($"Getting match request for clientid: {request.ClientId}");
+        Console.WriteLine($"Getting match request for clientid: {request.ClientId}");
 
-      var monsterState = new MonsterState {MonsterName = "Ben", XCoord = 0, YCoord = 2};
-      var fullState = new FullMonsterStateResponse();
-      fullState.AllMonsters.Add(monsterState);
-      return Task.FromResult(fullState);
+        var monsterState = new MonsterState { MonsterName = "Ben", XCoord = 0, YCoord = 2 };
+        var fullState = new FullMonsterStateResponse();
+        fullState.AllMonsters.Add(monsterState);
+        return Task.FromResult(fullState);
     }
-  }
-
-  class Program
-  {
-    private const int Port = 9091;
-
-    static void Main(string[] args)
-    {
-    ServerServiceDefinition gameServiceService = GameService.BindService(new GameServerImpl());
-
-      Server server = new Server
-      {
-        Services = {gameServiceService},
-        Ports = {new ServerPort("localhost", Port, ServerCredentials.Insecure)},
-      };
-      server.Start();
-
-      Console.WriteLine("Greeter server listening on port " + Port);
-
-      Console.WriteLine("Press any key to stop the server...");
-      Console.ReadKey();
-      server.ShutdownAsync().Wait();
-
-    }
-  }
 }
--- mean-monsters-server/ProtoHelpers.cs
+++ mean-monsters-server/ProtoHelpers.cs
@@ -1,13 +1,15 @@
 using System.IO;
 using Google.Protobuf;
 
-namespace mean_monsters_server {
-  public static class ProtoHelpers {
-    public static byte[] GetBytes<T>(this IMessage<T> proto) where T : IMessage<T>
+namespace mean_monsters_server
+{
+    public static class ProtoHelpers
     {
-      using var stream = new MemoryStream();
-      proto.WriteTo(stream);
-      return stream.ToArray();
+        public static byte[] GetBytes<T>(this IMessage<T> proto) where T : IMessage<T>
+        {
+            using var stream = new MemoryStream();
+            proto.WriteTo(stream);
+            return stream.ToArray();
+        }
     }
-  }
 }
